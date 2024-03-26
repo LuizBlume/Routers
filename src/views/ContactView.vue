f65940cd8dec8eb90bf1837ca9424f41353ea152
LuizBlume <luiz.fernando.251107@gmail.com>
2024-03-26 15:26:49 -0300

    first commit

---
+++ src/views/ContactView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="contact">
+    <h1>This is a contact page</h1>
+  </div>
+</template>
+<style scoped>
+@media (min-width: 1024px) {
+  .contact {
+    display: flex;
+    min-height: 100vh;
+    align-items: center;
+  }
+}
+</style>
